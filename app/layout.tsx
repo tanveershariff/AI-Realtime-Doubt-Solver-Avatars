03d773d1af4b8557731f75e359b687df38b380b4
tanveershariff <sharifftanveer78@gmail.com>
2025-09-06 13:32:27 +0530

    Updated layout.tsx and package-lock.json after UI changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,8 +14,8 @@ const fontMono = FontMono({
 
 export const metadata: Metadata = {
   title: {
-    default: "HeyGen Interactive Avatar SDK Demo",
-    template: `%s - HeyGen Interactive Avatar SDK Demo`,
+    default: "DoubtSolver AI",
+    template: `%s - DoubtSolver AI`,
   },
   icons: {
     icon: "/heygen-logo.png",
